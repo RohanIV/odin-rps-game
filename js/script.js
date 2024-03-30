1c161a478c9c86dbcb7c60f2e014c81a737dae6c
Rohan <151065519+RohanIV@users.noreply.github.com>
2024-03-30 13:03:51 +0000

    [UPDATE 2]
    - Game plays one round
    - Add loop feature & score

--- js/script.js
+++ js/script.js
@@ -1,37 +1,42 @@
-function get_computer_choice(){
+function get_computer_choice() {
     const rps = ["ROCK", "PAPER", "SCISSORS"];
 
     let cpu_choice = rps[Math.floor(Math.random() * rps.length)];
     console.log(cpu_choice);
 
     return cpu_choice;
-
-    
 }
 
-function get_player_choice(choice){
+function get_player_choice(choice) {
     let upper = choice.toUpperCase();
 
     console.log(upper);
     return upper;
 }
 
-function play_round(players_choice, compuetrs_choice){
-    let player_score = 0;
-    let computer_score = 0;
+function play_round(players_choice, compuetrs_choice) {
 
-   if(players_choice == compuetrs_choice){
-        return "It is a draw";
-   }else{
-    return "win";
-   }
-
-   console.log(choice);
-
-    // let players_choice = "rock";
+        if (players_choice == compuetrs_choice) {
+            return "It is a draw";
+        } else if (players_choice == "ROCK" && compuetrs_choice == "SCISSORS") {
+            return "Player Wins";
+        } else if (players_choice == "ROCK" && compuetrs_choice == "PAPER") {
+            return "Computer Wins";
+        } else if (players_choice == "PAPER" && compuetrs_choice == "ROCK") {
+            return "Player Wins";
+        } else if (players_choice == "PAPER" && compuetrs_choice == "SCISSORS") {
+            return "Computer Wins";
+        } else if (players_choice == "SCISSORS" && compuetrs_choice == "PAPER") {
+            return "Player Wins";
+        } else if (players_choice == "SCISSORS" && compuetrs_choice == "ROCK") {
+            return "Computer Wins";
+        }else{
+            return "Not Valid";
+        }
 }
 
-let players_choice = get_player_choice("");
+
+let players_choice = get_player_choice("paper");
 let computers_choice = get_computer_choice();
 
 console.log(play_round(players_choice, computers_choice));
